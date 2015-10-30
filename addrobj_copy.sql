a252295b0e48b0061bfd284574945edd3bda7097
lihachsk <kirill2001.91@mail.ru>
2015-10-30 23:53:17 +0300

    починил неработающий скрипт
    Signed-off-by: lihachsk <kirill2001.91@mail.ru>

--- addrobj_copy.sql
+++ addrobj_copy.sql
@@ -4,15 +4,15 @@
             nextid, offname, okato, oktmo, operstatus, parentguid, placecode, 
             plaincode, postalcode, previd, regioncode, shortname, startdate, 
             streetcode, terrifnsfl, terrifnsul, updatedate, ctarcode, extrcode, 
-            sextcode, livestatus, normdoc, worksarea)
+            sextcode, livestatus, normdoc)
 SELECT actstatus, aoguid, aoid, aolevel, areacode, autocode, centstatus, 
        citycode, code, currstatus, enddate, formalname, ifnsfl, ifnsul, 
        nextid, offname, okato, oktmo, operstatus, parentguid, placecode, 
        plaincode, postalcode, previd, regioncode, shortname, startdate, 
        streetcode, terrifnsfl, terrifnsul, updatedate, ctarcode, extrcode, 
-       sextcode, livestatus, normdoc, worksarea
+       sextcode, livestatus, normdoc
   FROM(select 
        ROW_NUMBER () over (partition by aoguid order by startdate desc, updatedate desc) as RwNo,
 	   *
-	   from path.addrobj )
+	   from path.addrobj ) src
 where src.RwNo = 1;
